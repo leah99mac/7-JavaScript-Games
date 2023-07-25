b08698bfec820e21f8511753ba9731f1e8201421
leah99mac <leah.m.mackay@gmail.com>
2023-07-24 21:50:07 -0400

    Memory Game

---
+++ Memory_Game/app.js
@@ -0,0 +1,100 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+    }
+}
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    if (cardsChosen[0] == cardsChosen[1] && cardsChosenIds[0] !== cardsChosenIds[1]) {
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+        cards[cardsChosenIds[0]].removeEventListener('click',flipCard)
+        cards[cardsChosenIds[1]].removeEventListener('click',flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[cardsChosenIds[0]].setAttribute('src', 'images/blank.png')
+        cards[cardsChosenIds[1]].setAttribute('src', 'images/blank.png')
+    }
+    resultDisplay.innerHTML = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+    if (cardsWon.length == (cardArray.length / 2)) {
+        resultDisplay.innerHTML = 'All Matches Found!'
+    }
+}
+
+createBoard()
